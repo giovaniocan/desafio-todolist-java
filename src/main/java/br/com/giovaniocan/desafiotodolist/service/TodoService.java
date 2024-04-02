23eb953a647a2e86d68b6018b0cf8e90b9a5b53e
giovaniocan <giovaniocan@gmail.com>
2024-04-02 09:57:42 -0300

    created todo service

---
+++ src/main/java/br/com/giovaniocan/desafiotodolist/service/TodoService.java
@@ -0,0 +1,41 @@
+package br.com.giovaniocan.desafiotodolist.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import br.com.giovaniocan.desafiotodolist.entity.Todo;
+import br.com.giovaniocan.desafiotodolist.repository.TodoRepository;
+
+@Service
+public class TodoService {
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list() {
+        Sort sort = Sort.by("prioridade").descending().and( // aqui eu to ordenando a minha consulta, primeiro pela
+                                                            // minha prioridade
+                Sort.by("nome").ascending()); // e depois pela ordem afabetica
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+
+    }
+
+    public List<Todo> relete(Long id) {
+        todoRepository.deleteById(id);
+        return list();
+    }
+}
