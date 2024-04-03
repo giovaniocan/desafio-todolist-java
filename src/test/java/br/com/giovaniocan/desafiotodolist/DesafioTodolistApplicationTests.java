89f6586f278b57cea9d0e617918a31dd2eec6f9c
giovaniocan <giovaniocan@gmail.com>
2024-04-03 09:25:06 -0300

    creted testCreateTodoSuccess test

--- src/test/java/br/com/giovaniocan/desafiotodolist/DesafioTodolistApplicationTests.java
+++ src/test/java/br/com/giovaniocan/desafiotodolist/DesafioTodolistApplicationTests.java
@@ -1,13 +1,40 @@
 package br.com.giovaniocan.desafiotodolist;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import br.com.giovaniocan.desafiotodolist.entity.Todo;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class DesafioTodolistApplicationTests {
+	@Autowired // injeção
+	private WebTestClient WebTestClient;
 
 	@Test
-	void contextLoads() {
+	void testCreateTodoSuccess() {
+		var todo = new Todo("todo 1", "desc todo 1", false, 1);
+	
+		WebTestClient
+			.post() // o metodo
+			.uri("/todos") // a url
+			.bodyValue(todo) // passando o body da requisicao
+			.exchange() // faznedo a chamada
+			.expectStatus().isOk() // espera o status do retorno
+			.expectBody()
+			.jsonPath("$").isArray() // esse "$" é para pegar o retorno do json
+			.jsonPath("$.length()").isEqualTo(1)
+			.jsonPath("$[0].nome").isEqualTo(todo.getNome())
+			.jsonPath("$[0].descricao").isEqualTo(todo.getDescricao())
+			.jsonPath("$[0].realizado").isEqualTo(todo.isRealizado())
+			.jsonPath("$[0].prioridade").isEqualTo(todo.getPrioridade());
+
+	}
+
+	@Test
+	void testCreateTodoFalure() {
 	}
 
 }
